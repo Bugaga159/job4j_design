027dd5305602a0e9f344fc0de5b16fc5589d92bc
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2020-11-29 12:26:30 +0300

    1. Что такое итератор. [#416105] - правки

--- src/main/java/ru/job4j/it/BackwardArrayIt.java
+++ src/main/java/ru/job4j/it/BackwardArrayIt.java
@@ -21,7 +21,6 @@ public class BackwardArrayIt implements Iterator<Integer> {
 		if (!hasNext()) {
 			throw new NoSuchElementException();
 		}
-		point++;
-		return data[data.length - point];
+		return data[data.length - 1 - point++];
 	}
 }
