8d45bace22ee0116dcea94e3c316b4c7eca1b63d
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-06-09 23:35:19 +0300

    1. Читаем файл конфигурации [#858]

---
+++ src/main/java/ru/job4j/io/Config.java
@@ -0,0 +1,48 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringJoiner;
+
+public class Config {
+	private final String path;
+	private final Map<String, String> values = new HashMap<>();
+
+	public Config(final String path) {
+		this.path = path;
+	}
+
+	public void load() {
+		try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+			read.lines()
+					.filter(e -> !e.contains("#") && e.contains("="))
+					.forEach(e -> {
+						String[] line = e.split("=");
+						values.put(line[0], line[1]);
+					});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String value(String key) {
+		return values.get(key);
+	}
+
+	@Override
+	public String toString() {
+		StringJoiner out = new StringJoiner(System.lineSeparator());
+		try (BufferedReader read = new BufferedReader(new FileReader(this.path))) {
+			read.lines().forEach(out::add);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return out.toString();
+	}
+
+	public static void main(String[] args) {
+		System.out.println(new Config("data/app.properties"));
+	}
+}
