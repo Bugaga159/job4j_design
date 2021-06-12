4702bbf9766b5942a70f45f57cd5a6615fa0e708
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-06-12 15:44:27 +0300

    2. Анализ доступности сервера. [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+	public void unavailable(String source, String target) {
+		try (BufferedReader in = new BufferedReader(new FileReader(source));
+			 PrintWriter out = new PrintWriter(
+					 new BufferedOutputStream(
+							 new FileOutputStream(target)))) {
+				StringBuilder sb = new StringBuilder();
+			String line;
+			while ((line = in.readLine()) != null) {
+				String[] lines = line.split(" ");
+				if (sb.length() == 0 && (lines[0].equals("400") || lines[0].equals("500"))) {
+					sb.append(lines[1]).append(" / ");
+				}
+				if (sb.length() > 0 && (lines[0].equals("200") || lines[0].equals("300"))) {
+					sb.append(lines[1]).append("; ");
+					out.println(sb);
+					sb = new StringBuilder();
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		Analysis an = new Analysis();
+		an.unavailable("./data/server.log", "./data/unavailable.csv");
+	}
+}
