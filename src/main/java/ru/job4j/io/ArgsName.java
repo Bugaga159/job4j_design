bb4882146758370b542a310d8c5fde2160900045
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-06-14 01:15:29 +0300

    5.1. Именованные аргументы [#295518]

---
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+	private final Map<String, String> values = new HashMap<>();
+
+	public String get(String key) {
+		return values.get(key);
+	}
+
+	private void parse(String[] args) {
+		if (args.length == 0) {
+			throw new IllegalArgumentException("Parameters is null!");
+		}
+		Arrays.stream(args).filter(e -> e.contains("="))
+				.forEach(e -> {
+					String[] line = e.split("=");
+					if (line.length < 2) {
+						throw new IllegalArgumentException("Value parameter is null!");
+					}
+					values.put(line[0].substring(1), line[1]);
+				});
+	}
+
+	public static ArgsName of(String[] args) {
+		ArgsName names = new ArgsName();
+		names.parse(args);
+		return names;
+	}
+
+	public static void main(String[] args) {
+		ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+		System.out.println(jvm.get("Xmx"));
+
+		ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+		System.out.println(zip.get("out"));
+	}
+}
