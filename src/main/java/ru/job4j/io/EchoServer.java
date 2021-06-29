2bb203b810fcb48b2a5f0e4e809bfacfa3068dec
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-06-29 22:26:53 +0300

    4. Slf4j - вывод exception. [#268853]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -1,11 +1,15 @@
 package ru.job4j.io;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class EchoServer {
-	public static void main(String[] args) throws IOException {
+	private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
+
+	public static void main(String[] args) {
 		try (ServerSocket server = new ServerSocket(9000)) {
 			while (!server.isClosed()) {
 				Socket socket = server.accept();
@@ -31,6 +35,8 @@ public class EchoServer {
 					}
 				}
 			}
+		} catch (IOException e) {
+			LOG.error("IOException", e);
 		}
 	}
 }
--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,16 +7,10 @@ public class UsageLog4j {
 	private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
 	public static void main(String[] args) {
-		byte num1 = 127;
-		short num2 = 32767;
-		char num3 = 'd';
-		int num4 = 133;
-		long num5 = 14214124124L;
-		float num6 = 123.4f;
-		double num7 = 224.4;
-		boolean num8 = true;
-
-		LOG.debug("byte:{}, short:{}, char:{}, int:{}, long:{}, float:{}, double:{}, boolean:{}",
-				num1, num2, num3, num4, num5, num6, num7, num8);
+		try {
+			throw new Exception("Not supported code");
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
 	}
 }
