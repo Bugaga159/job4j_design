a7440321388ec4e727bb76a34ea986b7501e07b9
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-06-29 21:51:34 +0300

    1. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,13 +12,23 @@ public class EchoServer {
 				try (OutputStream out = socket.getOutputStream();
 					 BufferedReader in = new BufferedReader(
 							 new InputStreamReader(socket.getInputStream()))) {
+					String answer = "";
 					for (String str = in.readLine(); str != null && !str.isEmpty(); str = in.readLine()) {
 						System.out.println(str);
-						if (str.contains("msg=Bye")) {
+						if (str.contains("msg=Hello")) {
+							answer = "Hello";
+						} else if (str.contains("msg=Exit")) {
+							answer = "Завершить работу сервера.";
 							server.close();
+						} else if (str.contains("msg=")) {
+							String[] msg = str.split("=");
+							answer = msg[1];
 						}
 					}
 					out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+					if (!answer.isEmpty()) {
+						out.write((answer + "\r\n").getBytes());
+					}
 				}
 			}
 		}
