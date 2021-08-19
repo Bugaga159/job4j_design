2406cff4114a19a93de4f327121f3a2bbf84a214
Панин Андрей Валерьевич <AVPanin@alfabank.ru>
2021-08-19 19:49:21 +0300

    1. Схема прав пользователи и роли. [#1093]

---
+++ data/shopSql/insert.sql
@@ -0,0 +1,19 @@
+insert into role(name) values ('Admin');
+insert into role(name) values ('support');
+insert into role(name) values ('customer');
+
+insert into rules(name) values ('create');
+insert into rules(name) values ('read');
+insert into rules(name) values ('update');
+insert into rules(name) values ('delete');
+insert into rules(name) values ('all');
+
+insert into category(name) values ('seo');
+insert into category(name) values ('qa');
+insert into category(name) values ('it');
+insert into category(name) values ('java');
+
+insert into state(name) values ('open');
+insert into state(name) values ('close');
+insert into state(name) values ('inprogress');
+insert into state(name) values ('transit');
